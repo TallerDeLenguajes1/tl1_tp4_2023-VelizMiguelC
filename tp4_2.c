8c42f3c2ef3c1ae84cba056bed67406451d530b8
VelizMiguelC <miniveliz14@gmail.com>
2023-04-22 16:22:50 -0300

    Avance

---
+++ tp4_2.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define TAMA 200
+
+struct Tarea {
+int TareaID; //Numerado en ciclo iterativo
+char *Descripcion;
+int Duracion; // entre 10 – 100
+}typedef tarea;
+
+void inicializararreglo(tarea **tareas,int canttareas);
+void cargartareas(tarea **tareas,int canttareas);
+void control(tarea **tareaspend,tarea **tareasrealiz,int canttareas);
+void mostrartareas(tarea **tareas,int canttareas);
+
+int main (){
+    int canttareas;
+    tarea **tareaspend;
+    tarea **tareasrealiz;
+    printf("Ingrese la cantidad de tareas que va a realizar:\n");
+    scanf("%d",&canttareas);
+    tareaspend=(tarea**)malloc(sizeof(tarea*)*canttareas);
+    tareasrealiz=(tarea**)malloc(sizeof(tarea*)*canttareas);
+    inicializararreglo(tareaspend,canttareas);
+    inicializararreglo(tareasrealiz,canttareas);
+    cargartareas(tareaspend,canttareas);
+    control(tareaspend,tareasrealiz,canttareas);
+    printf("-------------Tareas Pendientes-------------\n");
+    mostrartareas(tareaspend,canttareas);
+    printf("-------------Tareas Realizadas-------------\n");
+    mostrartareas(tareasrealiz,canttareas);
+}
+
+void inicializararreglo(tarea **tareas,int canttareas){
+    for (int i = 0; i < canttareas; i++)
+    {
+        tareas[i]=NULL;
+    }
+    
+}
+void cargartareas(tarea **tareas,int canttareas){
+    for (int i = 0; i < canttareas; i++)
+    {
+        char *buff=(char*)malloc(sizeof(char)*TAMA);
+        tareas[i] = (tarea*)malloc(sizeof(tarea));
+        tareas[i]->TareaID=i+1;
+        printf("TareaID:%d\n",i+1);
+        printf("Ingrese la descripcion de la tarea:\n");
+        scanf("%s",buff);
+        tareas[i]->Descripcion=(char*)malloc(sizeof(char)*(strlen(buff)+1));
+        strcpy(tareas[i]->Descripcion,buff);
+
+        free(buff);
+        printf("Ingrese la duraion de la tarea:\n");
+        scanf("%d",&tareas[i]->Duracion);
+    }
+    
+}
+void control(tarea **tareaspend,tarea **tareasrealiz,int canttareas){
+    int control;
+    printf("------------TareasPendientes------------\n");
+    for (int i = 0; i < canttareas; i++)
+    {
+        printf("TareaID=%d\n",i+1);
+        printf("Descripcion de tarea:%s\n",tareaspend[i]->Descripcion);
+        printf("Duracion de la tarea:%d\n",tareaspend[i]->Duracion);
+        printf("Se realizo la tarea?Si(1),No(0)\n");
+        scanf("%d",&control);
+        if (control==1)
+        {
+            tareasrealiz[i]=tareaspend[i];
+            tareaspend[i]=NULL;
+        }
+        
+    }
+    
+}
+void mostrartareas(tarea **tareas,int canttareas){
+    for (int i = 0; i < canttareas; i++)
+    {
+        if (tareas[i] != NULL)
+        {
+            printf("---Tarea numero %d---\n",i+1);
+            printf("Descripcion de tarea:%s\n",tareas[i]->Descripcion);
+            printf("Duracion de la tarea:%d\n",tareas[i]->Duracion);
+        }else{
+            printf("La tarea Num %d esta en NULL\n", i+1);
+        }
+        
+        
+    }
+    
+}
